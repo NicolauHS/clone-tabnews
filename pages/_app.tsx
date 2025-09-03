79d6a42b6c8d649d75f09381daee7cefde3a4c40
soares_nicolas <soares_nicolas@sicredi.com.br>
2025-09-03 10:46:23 -0300

    feat: install and config Material UI for project

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from "next/app";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+  },
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
